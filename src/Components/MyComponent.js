d5cc2ec4920a3540af5b7ed2051dd60b7b8eab85
Alex K from web-riot.com <kuvinci@gmail.com>
2020-07-09 16:24:49 +0300

    вум

--- src/Components/MyComponent.js
+++ src/Components/MyComponent.js
@@ -1,20 +1,16 @@
-import React, {Fragment} from 'react';
-import './MyComponent.scss';
+import React, { Fragment } from "react";
+import "./MyComponent.scss";
 
 class MyComponent extends React.Component {
-	render() {
-		console.log('wdwd')
-		// return 
-		// <Fragment>
-		// 	<h1>{this.props.title}</h1>
-		// 	<p></p>
-		// </Fragment>
-		//return React.createElement('h1', null, `Title: ${this.props.title}`)
-		return <div className="x">
-				{[1,2,3].map(item => <div>{item}</div>)}
-		</div>
-	}
+  render() {
+    return (
+      <div className="x">
+        {[1, 2, 3].map((item) => (
+          <div>{item}</div>
+        ))}
+      </div>
+    );
+  }
 }
-
 
 export default MyComponent;
